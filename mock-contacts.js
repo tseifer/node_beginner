69dd3c05153e979c59a792b3d7b1f4be138e627c
Tidhar <tseifer@yahoo.com>
2016-09-28 20:04:43 +0300

    first commit

--- mock-contacts.js
+++ mock-contacts.js
@@ -1,3 +1,4 @@
+'use strict'
 let contacts = {"data":[{"id":0,"name":"rotemxx","tel":"2309219387"},{"id":1,"name":"noam","tel":"34543"},{"id":2,"name":"maya","tel":"98789"},{"id":3,"name":"natali","tel":"239372039"}]};
 
 module.exports = contacts;
