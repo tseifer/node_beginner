4660f7a3b7275426dcc2e7e4e7913eb33b4d1149
Tidhar <tseifer@yahoo.com>
2016-09-28 19:06:37 +0300

    Initial commit

---
+++ db.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Tidhar on 21/9/2016.
+ */
+var mongoose = require('mongoose');
+
+var Contact = mongoose.model('Contact', {id: Number, name: String, tel: String});
+
+function init() {
+    mongoose.connect('mongodb://anyfont.info/myVirusDB');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+        console.log('DB Started');
+    });
+}
+
+function createDemoContacts(cb) {
+    let contact = new Contact({id: 1, name: 'Tidhar Seifer', tel: '0544523238'});
+    contact.save(cb);
+}
+
+function getContacts(cb) {
+    Contact.find({}, cb);
+}
+
+
+module.exports = {
+    'init': init,
+    'createDemoContacts': createDemoContacts,
+    'getContacts': getContacts
+
+
+};
+
+
+
---
+++ index.js
@@ -0,0 +1,38 @@
+let app = require('express')();
+let http = require('http');
+let server = http.createServer(app);
+let db = require('./db');
+let contacts = require('./mock-contacts');
+
+server.on('listening', onListening);
+
+
+server.listen(8080);
+
+db.init();
+
+app.get('/', function(req, res) {
+    res.send('HEllo from NOde.js');
+});
+app.get('/getContacts', function(req, res) {
+    console.log("called getContacts");
+    db.getContacts(function (err, allContacts) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(allContacts);
+            res.send(allContacts);
+        }
+    });
+});
+app.get('/createDemoContacts', function(req, res) {
+    db.createDemoContacts(function(err, contact) {
+        res.send({error: err, result: contact});
+    });
+});
+
+
+function onListening() {
+    var addr = server.address();
+    console.log('Node.JS listening on ' + addr + ':' + addr.port);
+}
